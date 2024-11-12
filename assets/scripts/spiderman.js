42f4c769bec0d078fda2ccf4fb05fde92f87702e
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-11-12 09:41:58 -0400

    andrew remove one li link child

--- assets/scripts/spiderman.js
+++ assets/scripts/spiderman.js
@@ -8,3 +8,14 @@ const imgLogo = document.querySelector('.s-logo img');
 if (imgLogo) {
     imgLogo.src = `../../assets/images/${spider}/${filme}/logo.png`;
 }
+
+if(spider === 'spiderman-andrew'){
+    const listaLinks = document.getElementById('listaLinks');
+    listaLinks.removeChild(listaLinks.lastElementChild);
+    console.log(listaLinks);
+}
+
+/*
+https://github.com/adrianocruzweb/desfio-multiverso-spiderman
+
+adicionei a troca de logo dinâmica mais vou trocar tudo dinamicamente por JS, não vai precisar replicar o arquivo da pagina interna. */
